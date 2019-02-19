7399cc95e20e62437681ec868f80fe37cfc48a4f
Damien Flury <damien.flury@gmail.com>
2019-02-19 11:16:01 +0100

    Add learn component

--- src/artemis/src/App.js
+++ src/artemis/src/App.js
@@ -10,7 +10,8 @@ import NavMenu from './shared/NavMenu'
 import Sets from './components/Sets'
 import SetDetail from './components/SetDetail'
 import { blue } from '@material-ui/core/colors'
-import Cards from './components/study/Cards';
+import Cards from './components/study/Cards'
+import Learn from './components/study/Learn'
 
 class App extends Component {
   state = {
@@ -43,7 +44,8 @@ class App extends Component {
               <Route exact path="/" component={Home} />
               <Route exact path="/sets" component={Sets} />
               <Route exact path="/sets/:id" component={SetDetail} />
-              <Route exact path="/cards/:id" component={Cards}/>
+              <Route exact path="/cards/:id" component={Cards} />
+              <Route exact path="/learn/:id" component={Learn} />
             </Switch>
           </NavMenu>
         </BrowserRouter>
